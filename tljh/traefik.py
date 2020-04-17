876e6e289ef71a2fcd4ec55321fe5405469eeecd
rooster1979 <p.o.redmond@se17.qmul.ac.uk>
2020-04-17 08:20:14 +0100

    cg

--- tljh/traefik.py
+++ tljh/traefik.py
@@ -57,7 +57,7 @@ def ensure_traefik_binary(prefix):
     )
     print(f"Downloading traefik {traefik_version}...")
     # download the file
-    response = requests.get(traefik_url, verify=false)
+    response = requests.get(traefik_url, verify=False)
     if response.status_code == 206:
         raise Exception("ContentTooShort")
     with open(traefik_bin, 'wb') as f:
